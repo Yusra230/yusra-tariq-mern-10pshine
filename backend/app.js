2e1cba1fa7a8d4893203bf7e40cc74ecb12c0ada
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2026-01-30 12:16:49 +0500

    Implement Notes APIs: routes, controllers, models, app setup, package dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,24 @@
-import express from 'express'
+
+//External module
+import express from "express"
 import mongoose from 'mongoose';
+const DB_PATH = "mongodb+srv://yusratariqdev_db_user:notesappp@notesapp.ejxi915.mongodb.net/";
+import cors from "cors"
+import noteItemRouter from "./routes/noteItemRouter.js";
+//local module
 
 const app = express();
-app.set("view engine", "ejs");
 
 app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+app.use('/api/notes',noteItemRouter)
+// app.use(errorController.pagenotfound);
 
 const PORT = 3000;
 
-const DB_path = "mongodb+srv://yusratariqdev_db_user:notesappp@notesapp.ejxi915.mongodb.net/";
-
-mongoose.connect(DB_path).then(() => {
+mongoose.connect(DB_PATH).then(() => {
   console.log("connected to mongo");
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
@@ -22,3 +26,4 @@ mongoose.connect(DB_path).then(() => {
 }).catch((err) => {
   console.log("error while connecting to mongo", err);
 })
+
